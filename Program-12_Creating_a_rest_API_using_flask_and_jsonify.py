ed039655369fc8f39c720145651c050885cd961c
Alpha4210 <alpha.gamer.2427@gmail.com>
2025-01-31 22:14:30 +0530

    Program-12 Created a rest api using flask and jsonify which shows the answer of the armstrong number in a json file

--- Program-12_Creating_a_rest_API_using_flask_and_jsonify.py
+++ Program-12_Creating_a_rest_API_using_flask_and_jsonify.py
@@ -2,39 +2,31 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "<p>Hello, World!</p>"
 
-@app.route('/armstrong/<int:n>')
-def armstrong(n):
-    sum = 0
-    order = len(str(n))
-    copy_n = n
-    while(n>0):
-        digit = n%10
-        sum += digit**order
-        n = n//10
+@app.route("/armstrong/<int:num>")
+def armstrong(num):
+    #Check if a number is an armstrong number - the numner is equal to the sum of the digits of the number raised to the power of the number of digits of the number.
+    x = 0 #Saves the number calculated for checking with the given number
     
-    if (sum==copy_n):
-        print(f"{copy_n} is an armstrong number")
-        result={
-            "number": n,
-            "Armstrong":True,
-            "server ip":"197.45.33.01"
-        }
+    # Calculating the number
     
+    for i in str(num):
+        x = int(i)**3+x
+           
+    # Checking if its armstrong number
+    if x==int(num):
+        print(f"{num} is an armstrong number")
+        return jsonify(armstrong = True,
+                       number = num,
+                       )
     else:
-        print(f"{copy_n} is not an armstrong number")
-        result={
-            "number": n,
-            "Armstrong":False,
-            "server ip":"197.45.33.01"
-        }
-    return jsonify(result)
-            
-
+        print("It is not a armstrong number")
+        return jsonify(armstrong = False,
+                       number = num,
+                        )
+                
 if __name__=="__main__":
     app.run(debug=True)
-    
-    
